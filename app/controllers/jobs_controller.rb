32bcd637284dc0c7c0aaccaf9694cf5994bc1b5c
Cherry Shen <cshen@wellesley.edu>
2015-10-03 15:19:10 -0400

    allow user to delete jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -23,6 +23,12 @@ class JobsController < ApplicationController
 		redirect_to jobs_path
 	end
 
+	def destroy
+		@job = Job.find(params[:id])
+		@job.destroy
+		redirect_to jobs_path
+	end
+
 	private
 	def job_params
 		params.require(:job).permit(:title, :description)
